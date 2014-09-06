81145f2b58719a867c00171ffdb013995271332a
Kenta Sato <stkn417@gmail.com>
2014-09-06 15:21:01 +0900

    Add form

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
+require 'sinatra'
+require 'slim'
+
 class App < Sinatra::Base
   register Sinatra::Reloader
 
   get '/' do
+    @link = ""
+    @code = ""
+    slim :index
+  end
+
+  post '/' do
+    @link = @params[:link]
+    @code = @params[:link]
     slim :index
   end
 end
